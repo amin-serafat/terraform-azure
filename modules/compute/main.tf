4a27286947e94224e8288e4ef41bbe4cde9734cd
Amin Serafat <30317652+amin-serafat@users.noreply.github.com>
2025-06-11 19:58:39 +0300

    Create main.tf

---
+++ modules/compute/main.tf
@@ -0,0 +1,50 @@
+resource "azurerm_network_interface" "this" {
+  name                = "${var.env}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+}
+
+resource "azurerm_network_interface_security_group_association" "this" {
+  network_interface_id      = azurerm_network_interface.this.id
+  network_security_group_id = var.nsg_id
+}
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name                = "${var.env}-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.this.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = var.image.publisher
+    offer     = var.image.offer
+    sku       = var.image.sku
+    version   = var.image.version
+  }
+}
+
+output "public_ip" {
+  value = azurerm_network_interface.this.ip_configuration[0].private_ip_address
+}
