aee3dbd8756aaf735867746cf73bc4fe300de2cf
Amin Serafat <30317652+amin-serafat@users.noreply.github.com>
2025-06-11 20:03:18 +0300

    Create variables.tf

---
+++ modules/compute/variables.tf
@@ -0,0 +1,48 @@
+variable "env" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "nsg_id" {
+  type = string
+}
+
+variable "image" {
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+  default = {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
