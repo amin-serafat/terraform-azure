2bc884f4f3dfb8076a28e02b5273bf0ac85b2d9d
Amin Serafat <30317652+amin-serafat@users.noreply.github.com>
2025-06-11 20:04:37 +0300

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnets" {
+  type = list(string)
+}
